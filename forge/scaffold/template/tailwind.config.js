51beb3d938b5fc272e2c73dfedd825585ceef7a9
Dave Gaeddert <dave.gaeddert@gmail.com>
2022-03-31 16:47:00 -0500

    Add .heroku forge packages to Tailwind content

--- forge/scaffold/template/tailwind.config.js
+++ forge/scaffold/template/tailwind.config.js
@@ -3,8 +3,8 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   content: [
     "./app/**/*.{html,js}",
-    // Include the forge templates too
-    ".venv/lib/python*/site-packages/forge/**/*.{html,js}"
+    // Include the forge package templates (in development and production)
+    "./{.venv,.heroku/python}/lib/python*/site-packages/forge*/**/*.{html,js}",
   ],
   theme: {
     extend: {},
