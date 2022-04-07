9192ee0f85a5a1bbba16fafbff86c8952d1b73bd
Dave Gaeddert <dave.gaeddert@gmail.com>
2022-04-07 11:17:06 -0500

    Setup commands

--- forge/cli.py
+++ forge/cli.py
@@ -1,11 +1,13 @@
 import glob
 import os
+import platform
 import shutil
 import subprocess
 import sys
 
 import click
 import dj_database_url
+from django.core.management.utils import get_random_secret_key
 from dotenv import dotenv_values, load_dotenv
 from honcho.manager import Manager as HonchoManager
 
@@ -108,6 +110,7 @@ forge pre-commit"""
             print("pre-commit hook installed")
     else:
 
+        # TODO implement the rest of these old pre-commit steps
         """#!/bin/sh -e
 
         BOLD="\033[1m"
@@ -183,6 +186,7 @@ def work():
 
     # Meant to work with Forge Pro, but doesn't necessarily have to
     if "STRIPE_WEBHOOK_PATH" in dotenv:
+        # TODO check stripe command available, need to do the same with docker
         django_env["STRIPE_WEBHOOK_SECRET"] = (
             subprocess.check_output(["stripe", "listen", "--print-secret"])
             .decode()
@@ -218,9 +222,14 @@ def work():
     sys.exit(manager.returncode)
 
 
-@cli.command()
+@cli.group()
+def setup():
+    pass
+
+
+@setup.command()
 @click.pass_context
-def init(ctx):
+def template(ctx):
     """Forge is already installed, and presumably in a git repo."""
 
     # Matches the format in quickstart.py
@@ -255,6 +264,82 @@ def init(ctx):
     # technically this will give an error code because db isn't running
     event("Creating default team and user migrations")
     Forge().manage_cmd("makemigrations", stderr=subprocess.DEVNULL)
+
+
+@setup.command()
+@click.option("--postgres-tier", default="hobby-dev")
+@click.option("--redis-tier", default="hobby-dev")
+@click.option("--team", default="")
+@click.argument("heroku_app_name")
+@click.pass_context
+def heroku(ctx, heroku_app_name, postgres_tier, redis_tier, team):
+    if (
+        subprocess.call(
+            ["git", "remote", "show", "heroku"],
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
+        )
+        == 0
+    ):
+        click.secho("heroku remote already exists", fg="red", err=True)
+        sys.exit(1)
+
+    if team:
+        click.secho(f"Creating Heroku app on {team}", bold=True)
+        subprocess.check_call(
+            ["heroku", "apps:create", heroku_app_name, "--team", team]
+        )
+    else:
+        click.secho("Creating Heroku app", bold=True)
+        subprocess.check_call(["heroku", "apps:create", heroku_app_name])
+
+    click.secho("Setting Heroku buildpacks", bold=True)
+    subprocess.check_call(["heroku", "buildpacks:clear"])
+    subprocess.check_call(
+        [
+            "heroku",
+            "buildpacks:add",
+            "https://github.com/django-forge/heroku-buildpack-forge.git",
+        ]
+    )
+    subprocess.check_call(["heroku", "buildpacks:add", "heroku/nodejs"])
+    subprocess.check_call(
+        [
+            "heroku",
+            "buildpacks:add",
+            "https://github.com/moneymeets/python-poetry-buildpack.git",
+        ]
+    )
+    subprocess.check_call(["heroku", "buildpacks:add", "heroku/python"])
+
+    click.secho("Adding Postgres and Redis", bold=True)
+    subprocess.check_call(
+        ["heroku", "addons:create", f"heroku-postgresql:{postgres_tier}"]
+    )
+    subprocess.check_call(["heroku", "addons:create", f"heroku-redis:{redis_tier}"])
+
+    click.secho("Setting PYTHON_RUNTIME_VERSION, SECRET_KEY, and BASE_URL", bold=True)
+    python_version = platform.python_version()
+    secret_key = get_random_secret_key()
+    # TODO --domain option?
+    base_url = f"https://{heroku_app_name}.herokuapp.com"
+    subprocess.check_call(
+        [
+            "heroku",
+            "config:set",
+            f"PYTHON_RUNTIME_VERSION={python_version}",
+            f"SECRET_KEY={secret_key}",
+            f"BASE_URL={base_url}",
+        ]
+    )
+
+    click.secho("Enabling runtime-dyno-metadata", bold=True)
+    subprocess.check_call(["heroku", "labs:enable", "runtime-dyno-metadata"])
+
+    click.secho(
+        f"You're all set! Connect your GitHub repo to the Heroku app at:\n\n  https://dashboard.heroku.com/apps/{heroku_app_name}/deploy/github",
+        fg="green",
+    )
 
 
 if __name__ == "__main__":
