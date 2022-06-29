dcfcedf2e5c5f80dfb4c32112c623df23ea8173e
Dave Gaeddert <dave.gaeddert@gmail.com>
2022-06-29 11:25:21 -0500

    Exclude STATIC_URL from logging

--- forge/logging.py
+++ forge/logging.py
@@ -1,18 +1,25 @@
 import logging
 
-EXCLUDE_URLS = [
-    "/static/",
-    "/admin/jsi18n/",
-    "/favicon.ico",
-]
+from django.conf import settings
 
 
 class ExcludeCommonURLsFilter(logging.Filter):
+    def __init__(self):
+        self.exclude_urls = [
+            "/admin/jsi18n/",  # could be customized...
+            "/favicon.ico",
+        ]
+
+        if settings.STATIC_URL:
+            self.exclude_urls.append(settings.STATIC_URL)
+
+        super().__init__()
+
     def filter(self, record):
         message = record.getMessage()
 
         if message.startswith('"GET /'):  # Only check them all if it's a GET
-            for url in EXCLUDE_URLS:
+            for url in self.exclude_urls:
                 if message.startswith(f'"GET {url}'):
                     return False
 
